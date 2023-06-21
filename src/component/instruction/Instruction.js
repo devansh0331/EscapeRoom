a3480dd3c94a632ff5cad45421143cd08e1084bc
devansh0331 <devanshshrivastava07@gmail.com>
2023-06-21 23:43:05 +0530

    setup

---
+++ src/component/instruction/Instruction.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import "./Instruction.css"
+
+function Instruction() {
+  return (
+    <div className='instruction'>
+      
+      <div className='insContent'>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Instruction
