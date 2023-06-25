59cb4e9bbb06cab65ba5115685f5568187189c8a
RITESHKUMARTHAKUR <thakurram2668@gmail.com>
2023-06-25 09:31:55 +0530

    initial commit

--- src/component/Instructions/Instructions.js
+++ src/component/Instructions/Instructions.js
@@ -1,22 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Instructions = () => {
   return (
     <div className='bg-slate-900 h-screen text-white '>
-      <div className="h-full w-[80%] m-auto">
-        <h3 className='text-center'>Escape Room</h3>
-        <div className="main-box  flex items-center  justify-evenly">
-          <h2 className=' pr-4 self-center w-[40%] text-center text-2xl'>Instructions</h2> 
-          <ul className=' border-l-2 border-white self-center text-center w-[60%]'>
+      <div className=" flex flex-col h-full">
+        <h3 className='text-center pt-8 text-2xl'>Escape Room</h3>
+        <div className="  flex pt-28 items-center justify-evenly text-[1.3em]">
+          <h2 className='  w-[30%] text-center text-[1.5em]  flex-grow'>Instructions</h2> 
+          <div className='border-l-2 border-white w-[70%]'>
+          {/* list-disc */}
+          <ul className='pl-12  '> 
             <li className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, magni?</li>
             <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, magni?</li>
             <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, magni?</li>
             <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, magni?</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, magni?</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, magni?</li>
           </ul>
+          </div>
         </div>
-        <div className="button text-center my-6" >
-        <button class="flex-shrink-0 text-white bg-violet-700 border-0 py-1 px-10 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0">Button  </button>
-        </div>
+        <div className="button text-center mt-16">
+          
+        <Link to="/escaperoom" className='flex-shrink-0 text-white bg-violet-700 border-0 py-2 px-10 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0'>Start </Link> 
+          </div>
       </div>
     </div>
   )
