76edc3b74c3a209c1b3c07cd1b83a72dca3a6462
Yukti Jhawar <yuktimj2002@gmail.com>
2023-07-07 17:56:33 +0530

    instructions css

--- src/component/Instructions/Instructions.js
+++ src/component/Instructions/Instructions.js
@@ -6,21 +6,21 @@ import { useNavigate } from "react-router-dom";
 // import {signInWithPopup} from "firebase/auth"
 
 import './Instruction.css'
+import paper2 from '../../img/paper2.png'
 
 const Instructions = () => {
   const navigate = useNavigate()
 
   return (
     <div className='instruction'>
-     <div className='main'>
-      <p className='paper'>
+          <img src={paper2} className='paper'/>
+          <main className='text-wrapper'>
           <h2 className='title'>ESCAPE ROOM</h2>
           <p className='subtitle'>Escape the room using the hidden clues</p>
           <p className='subtitle s2'><b>Note:</b> Only limited participants will be entering the next level ☠ </p>
           <button onClick={()=>{navigate('./coundown')}} className='btnStart'>Start</button>
           <p className='subtitle s3'>(You can google the riddles if required)</p> 
-      </p>
-     </div>
+          </main>
     </div>
   )
 }
